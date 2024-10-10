98151c509139b9c5f6ef6ec7400fa22861ca0457
dharmikDDs <dharmikrokad.dds@gmail.com>
2024-10-10 09:18:59 +0530

    + new colors.

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -8,9 +8,20 @@ class AppColors {
   static const Color darkRedBorderColor = Color(0xff3c0202);
   static const Color transparent = Colors.transparent;
   static const Color yellowAccent = Colors.yellowAccent;
+  static const Color greyColor = Colors.grey;
 
   static const Color primary = Color(0xffA02334);
   static const Color secondary = Color(0xffFFAD60);
   static const Color ternary = Color(0xffFFEEAD);
   static const Color bg = Color(0xff96CEB4);
+
+  static const Color color1 = Color(0xfffdc051);
+  static const Color color1l = Color(0xffffd851);
+  static const Color color2 = Color(0xfff14d54);
+  static const Color color2l = Color(0xffff5151);
+  static const Color color3 = Color(0xff023fae);
+  static const Color color3l = Color(0xff025afa);
+  static const Color color4 = Color(0xff4f3739);
+  static const Color color4l = Color(0xffc1858a);
+  static const Color color5 = Color(0xfffff8e7);
 }
